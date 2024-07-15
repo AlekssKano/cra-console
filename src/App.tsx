baf75b022138498ae39095924eea90526f8c15fb
AlekssKano <aleksskano@gmail.com>
2024-07-15 23:56:02 +0930

    Maket1

--- src/App.tsx
+++ src/App.tsx
@@ -1,115 +1,98 @@
 import React from 'react';
 import styled, {css} from "styled-components";
-import {StyledButton} from "./components/Button.styled";
 
-//{<input type='text'/>}
-
-
-//"https://s3-alpha-sig.figma.com/img/295b/9b3e/0cbd17e4381497547009ce81acc4eee3?Expires=1720396800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=a-I6roRvxYqBTKfyGdvU7jUvJoy1Lop2iHBqiNwVSrxE4tBLGlWCB58dEGEnTGGl71dLUu1PZvnuzhULJ0Cu7QFrrfzhOsKrBhYIfNlb2AYLIch6y44qRGpOFMSFL2XC45zsK69xY9rzTts0~nU4okR2DXa8nyV6SDj0amiTb51PQlNqNx~wuoVdic-55EBxQV0vDnqW2KHQEe1e~j6lb1gH3NFwNIt9ey4uGSSLT3v-On-35xhXgWprNIDrafn3rYQdUkuCLsfTzucfZvZ1eWtRJlJuBbSXdUaUetHRWBGOsamOF2OmfNG~Y5-mNR1FO8EOybyz83Nh6pqeiCpbHQ__"
 function App() {
     return (
-        <div className="App">
 
-            <Field>
-                <Card>
-                    <Rectangle>
-                        <HeaderImage
-                            src="https://s3-alpha-sig.figma.com/img/295b/9b3e/0cbd17e4381497547009ce81acc4eee3?Expires=1720396800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=a-I6roRvxYqBTKfyGdvU7jUvJoy1Lop2iHBqiNwVSrxE4tBLGlWCB58dEGEnTGGl71dLUu1PZvnuzhULJ0Cu7QFrrfzhOsKrBhYIfNlb2AYLIch6y44qRGpOFMSFL2XC45zsK69xY9rzTts0~nU4okR2DXa8nyV6SDj0amiTb51PQlNqNx~wuoVdic-55EBxQV0vDnqW2KHQEe1e~j6lb1gH3NFwNIt9ey4uGSSLT3v-On-35xhXgWprNIDrafn3rYQdUkuCLsfTzucfZvZ1eWtRJlJuBbSXdUaUetHRWBGOsamOF2OmfNG~Y5-mNR1FO8EOybyz83Nh6pqeiCpbHQ__"
-                            alt="card image"/>
-                        <TextHeader>Headline </TextHeader><br/>
-                        <Text>Faucibus. Faucibus. Sit sit sapien sit tempusrisu ut. Sit molestie ornare in
-                            venen.<br/></Text>
-                        <StyledButton backgroundColor={'blue'} color={'snow'}>See More</StyledButton>
-                        <StyledButton backgroundColor={'white'} color={'blue'}>Save</StyledButton>
+        <StyledBody>
+            <StyledGrid>
+                <GridItem>1</GridItem>
+                <GridItem>2</GridItem>
+                <GridItem>3</GridItem>
+                <GridItem>4</GridItem>
+                <GridItem>5</GridItem>
+                <GridItem>6 </GridItem>
+                <GridItem>7</GridItem>
+                <GridItem>8</GridItem>
+            </StyledGrid>
+        </StyledBody>
 
-                    </Rectangle>
-
-
-                </Card>
-            </Field>
-        </div>
     );
 }
 
 export default App;
-const Field = styled.div`
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+
+const StyledBody = styled.section`
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 2rem;
+    padding: 15px;
+
+
+`
+
+const StyledGrid = styled.div`
+    display: grid;
+    grid-template-columns:  repeat(4, 265px);
+    grid-auto-rows: minmax(200px, auto);
     gap: 20px;
-    background-color: #f0f6f0;
+    //    grid-template-areas:
+    //"one two three four"
+    //"five five six eight"
+    //"five five seven eight";
+    // row-gap:10px;
+    //column-gap: 30px;
 `
-const Card = styled.div`
-    width: 300px;
-    height: 350px;
-    top: -317px;
-    left: -409px;
-   
-    border-radius: 15px 15px 15px 15px;
-    background: #FFFFFF;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    box-shadow: 0px 4px 20px 5px #0000001A;
-
-
-
-`
-const Rectangle = styled.div`
-    width: 280px;
-    height: 330px;
-    top: -317px;
-    left: -409px;
-    gap: 0px;
-    border-radius: 10px 10px 10px 10px;
-    background-color: #ffffff;
-`
-// const Photo = styled.div`
-// <div className="png">
-// <img src="https://s3-alpha-sig.figma.com/img/295b/9b3e/0cbd17e4381497547009ce81acc4eee3?Expires=1720396800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=a-I6roRvxYqBTKfyGdvU7jUvJoy1Lop2iHBqiNwVSrxE4tBLGlWCB58dEGEnTGGl71dLUu1PZvnuzhULJ0Cu7QFrrfzhOsKrBhYIfNlb2AYLIch6y44qRGpOFMSFL2XC45zsK69xY9rzTts0~nU4okR2DXa8nyV6SDj0amiTb51PQlNqNx~wuoVdic-55EBxQV0vDnqW2KHQEe1e~j6lb1gH3NFwNIt9ey4uGSSLT3v-On-35xhXgWprNIDrafn3rYQdUkuCLsfTzucfZvZ1eWtRJlJuBbSXdUaUetHRWBGOsamOF2OmfNG~Y5-mNR1FO8EOybyz83Nh6pqeiCpbHQ__"
-// alt=""/>
-// <span>PNG</span>
-// </div>`
-const HeaderImage = styled.img`
-    width: 280px;
-    height: 170px;
-    top: 10px;
-    left: 10px;
+const GridItem = styled.div`
+    background-color: #69B7E4;
     
- border-radius: 10px 10px 10px 10px; 
-margin-bottom: 20px;
-`
-const TextHeader = styled.span`
-    margin-left: 10px;
-    font-family: Inter,serif;
-    font-size: 16px;
-    font-weight: 700;
-    line-height: 30px;
-    text-align: left;
-    width: 70px;
-    height: 19px;
-    top: 200px;
-    left: 20px;
-    gap: 0px;
-    opacity: 0px;
-    color: #000000;
-    
+       
+
+    &:nth-child(5) {
+        grid-column-start: 1;
+        grid-column-end: 3;
+        grid-row-start: 2;
+        grid-row-end: 4; 
+    }
+    //
+    //&:nth-child(6) {
+    //    grid-area: 2/2/3/4;
+    //    background-color: rgba(251, 0, 227, 0.5);
+    //}
+    //
+    &:nth-child(8) {
+        grid-column: 4/5;
+        grid-row: 2/4;
+        grid-area: 2/4/span 2/5;
+    }
 
 
-`
-const Text = styled.span`
-    padding-left: 10px;
-    width: 260px;
-    height: 40px;
-    top: 239px;
-    left: 20px;
-    gap: 0px;
-    font-family: Inter,serif;
-    font-size: 12px;
-    font-weight: 500;
-    line-height: 20px;
-    text-align: left;
-    color: #ABB3BA;
-    
+    //&:first-child {
+    //    grid-area: one;
+    //}
+    //&:nth-child(2) {
+    //    grid-area: two;
+    //}
+    //&:nth-child(3) {
+    //    grid-area: three;
+    //}
+    //
+    //&:nth-child(4) {
+    //    grid-area: four;
+    //}
+    //
+    //&:nth-child(5) {
+    //    grid-area: five;
+    //}
+    //
+    //&:nth-child(6) {
+    //    grid-area: six;
+    //}
+    //
+    //&:nth-child(7) {
+    //    grid-area: seven;   
+    //}
+    //
+    //&:last-child  {
+    //    grid-area: eight;
+    //}
 `
